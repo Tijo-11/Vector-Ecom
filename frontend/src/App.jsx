a7780aadfa344435dfe1bf011fffe930b9cfe9bd
Tijo-11 <tijot111@gmail.com>
2025-08-13 13:54:36 +0530

    Initial Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+// src/App.jsx
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-indigo-500 to-purple-500">
+      <h1 className="text-4xl font-bold text-white">🎉 Tailwind is working!</h1>
+    </div>
+  );
+}
+
+export default App;
