a80ef1af4d77f0dd8402f7dae5cd97c35cfed33f
Tijo-11 <tijot111@gmail.com>
2026-02-21 18:43:42 +0530

    fix media serving for product/category images on production

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -39,9 +39,8 @@ urlpatterns = [
     path('api/admin/', include('admin_panel.urls')),
     
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
-urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
