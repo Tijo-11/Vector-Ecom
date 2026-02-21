d76c0bfb50a3e0af44018a1a534cca7a0273d3f8
Tijo-11 <tijot111@gmail.com>
2026-02-21 07:29:45 +0530

    Fix15

--- frontend/src/views/vendor/Shop.jsx
+++ frontend/src/views/vendor/Shop.jsx
@@ -6,7 +6,7 @@ import apiInstance from "../../utils/axios";
 import { CartContext } from "../../plugin/Context";
 import { addToWishlist } from "../../plugin/addToWishlist";
 import { addToCart } from "../../plugin/AddToCart";
-import CartID from "../shop
+import CartID from "../shop/ProductDetail/CartId";
 import GetCurrentAddress from "../shop/ProductDetail/UserCountry";
 import UserData from "../../plugin/UserData";
 import log from "loglevel";
